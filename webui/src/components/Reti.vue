54420aca57497b74b89497f42458ee99fd97fdbb
Andy Funk <acfunk@gmail.com>
2024-10-21 17:46:10 -0600

    aot

--- webui/src/components/Reti.vue
+++ webui/src/components/Reti.vue
@@ -1,14 +1,17 @@
 <template>
   <v-dialog v-model="show" max-width="400" persistent>
     <v-card>
+      <v-progress-linear indeterminate v-show="loading" class="mb-n1" />
       <v-card-title class="d-flex">
         Reti Validator
         <v-spacer />
         <v-icon color="currentColor" :icon="mdiClose" @click="show = false" />
       </v-card-title>
       <v-container>
-        <div>Latest: {{ latest }}</div>
-        <div>Version: {{ version }}</div>
+        <div :class="loading ? 'text-grey' : ''">
+          <div>Latest: {{ version.latest }}</div>
+          <div>Current: {{ version.current }}</div>
+        </div>
       </v-container>
     </v-card>
   </v-dialog>
@@ -28,22 +31,34 @@ const show = computed({
   },
   set(val) {
     if (!val) {
+      version.value = { latest: undefined, current: undefined };
       emit("close");
     }
   },
 });
 
-const latest = ref();
-const version = ref();
+const loading = ref(false);
+const version = ref({ latest: undefined, current: undefined });
 
-onBeforeMount(async () => {
-  const releases = await axios({
-    url: "https://api.github.com/repos/TxnLab/reti/releases",
-  });
-  latest.value = releases.data[0].tag_name;
-  const resp = await axios({
-    url: "http://localhost:3536/reti/version?latest=" + latest,
-  });
-  version.value = resp.data.slice(27, 27 + resp.data.slice(27).indexOf(" "));
-});
+watch(
+  () => show.value,
+  async (val) => {
+    if (val) {
+      loading.value = true;
+      const releases = await axios({
+        url: "https://api.github.com/repos/TxnLab/reti/releases",
+      });
+      version.value.latest = releases.data[0].tag_name;
+      const resp = await axios({
+        url:
+          "http://localhost:3536/reti/version?latest=" + version.value.latest,
+      });
+      version.value.current = resp.data.slice(
+        27,
+        27 + resp.data.slice(27).indexOf(" ")
+      );
+      loading.value = false;
+    }
+  }
+);
 </script>
